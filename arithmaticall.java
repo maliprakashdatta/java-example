296aeef400d7186d3e365d0a3b41eefc3c8aef35
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-03-26 08:45:48 +0530

    Arithmatic java program

---
+++ arithmaticall.java
@@ -0,0 +1,19 @@
+class arithmaticall
+{
+  public static void main(String args[])
+    {
+       int a=10;
+       int b=20;
+       int c;
+       c=a+b;
+       System.out.println("Add value:-"+c);
+	c=a-b;
+       System.out.println("substract value:-"+c);
+        c=a*b;
+       System.out.println("Multi value:-"+c);
+       c=a/b;
+       System.out.println("division value:-"+c);
+       c=a%b;
+       System.out.println("moduels vale:-"+c);
+    }
+}
